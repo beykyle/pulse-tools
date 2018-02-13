2b6983bb88a79ef92852b85f0bb22bb2d7ef09ab
Kyle Beyer <beykyle@umich.edu>
2018-02-12 21:07:42 -0500

    Added calibration for experimental data

--- rescon.py
+++ rescon.py
@@ -8,11 +8,15 @@ rescon.py: Code to find the energy resolution of an organic scintillator by iter
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+from matplotlib.widgets  import RectangleSelector
+from matplotlib.patches import Rectangle
 import waveform
 import dataloader
 import getwavedata
 from scipy.ndimage.filters import gaussian_filter1d as gfilter
 from scipy.optimize import curve_fit as fit
+from scipy.signal import savgol_filter as savgol
+from scipy.signal import find_peaks_cwt as peaks
 
 __author__ = "Kyle Beyer"
 __version__ = "1.0.1"
@@ -20,26 +24,195 @@ __maintainer__ = "Kyle Beyer"
 __email__ = "beykyle@umich.edu"
 __status__ = "Development"
 
+def booleanize(value):
+    """Return value as a boolean."""
+    true_values = ("yes", "true", "1")
+    false_values = ("no", "false", "0")
+    if isinstance(value, bool):
+        return value
+    if value.lower() in true_values:
+        return True
+    elif value.lower() in false_values:
+        return False
+    raise TypeError("Cannot booleanize ambiguous value '%s'" % value)
+
+def line_select_callback(eclick, erelease):
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+    rect = plt.Rectangle( (min(x1,x2),min(y1,y2)), np.abs(x1-x2), np.abs(y1-y2) )
+    ax.add_patch(rect)
+
+class DraggableRectangle:
+    lock = None  # only one can be animated at a time
+    def __init__(self ):
+        self.rect = Rectangle((0,0) ,1 , 1 )
+        self.press = None
+        self.background = None
+
+    def connect(self):
+        'connect to all the events we need'
+        self.cidpress = self.rect.figure.canvas.mpl_connect(
+            'button_press_event', self.on_press)
+        self.cidrelease = self.rect.figure.canvas.mpl_connect(
+            'button_release_event', self.on_release)
+        self.cidmotion = self.rect.figure.canvas.mpl_connect(
+            'motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        'on button press we will see if the mouse is over us and store some data'
+        if event.inaxes != self.rect.axes: return
+        if DraggableRectangle.lock is not None: return
+        contains, attrd = self.rect.contains(event)
+        if not contains: return
+        print('event contains', self.rect.xy)
+        x0, y0 = self.rect.xy
+        self.press = x0, y0, event.xdata, event.ydata
+        DraggableRectangle.lock = self
+
+        # draw everything but the selected rectangle and store the pixel buffer
+        canvas = self.rect.figure.canvas
+        axes = self.rect.axes
+        self.rect.set_animated(True)
+        canvas.draw()
+        self.background = canvas.copy_from_bbox(self.rect.axes.bbox)
+
+        # now redraw just the rectangle
+        axes.draw_artist(self.rect)
+
+        # and blit just the redrawn area
+        canvas.blit(axes.bbox)
+
+    def on_motion(self, event):
+        'on motion we will move the rect if the mouse is over us'
+        if DraggableRectangle.lock is not self:
+            return
+        if event.inaxes != self.rect.axes: return
+        x0, y0, xpress, ypress = self.press
+        dx = event.xdata - xpress
+        dy = event.ydata - ypress
+        self.rect.set_x(x0+dx)
+        self.rect.set_y(y0+dy)
+
+        canvas = self.rect.figure.canvas
+        axes = self.rect.axes
+        # restore the background region
+        canvas.restore_region(self.background)
+
+        # redraw just the current rectangle
+        axes.draw_artist(self.rect)
+
+        # blit just the redrawn area
+        canvas.blit(axes.bbox)
+
+    def on_release(self, event):
+        'on release we reset the press data'
+        if DraggableRectangle.lock is not self:
+            return
+
+        self.press = None
+        DraggableRectangle.lock = None
+
+        # turn off the rect animation property and reset the background
+        self.rect.set_animated(False)
+        self.background = None
+
+        # redraw the full figure
+        self.rect.figure.canvas.draw()
+
+    def disconnect(self):
+        'disconnect all the stored connection ids'
+        self.rect.figure.canvas.mpl_disconnect(self.cidpress)
+        self.rect.figure.canvas.mpl_disconnect(self.cidrelease)
+        self.rect.figure.canvas.mpl_disconnect(self.cidmotion)
+
 class Experiment:
-    def __init__(self , energy):
-        self.energy = energy
+    def __init__(self , lines , bins, *args , **kwargs):
+        if kwargs is not None:
+                for key, value in kwargs.items():
+                    if key == "readSpec":
+                        self.readSpec = booleanize(value)
+
+        self.bins  = int(bins)
+        self.lines = lines
+
+    def run(self , config):
+        if(hasattr(self , 'readSpec')):
+            if(self.readSpec == True):
+                spec = []
+                volts = []
+                #iterate through every file with spec*.tmp
+                #read each channel of spec and volts
+        else:
+            volts , spec = self.readFromFile(config)
+            self.writeSpec(volts , spec)
+
+        edges = []
+        energy = []
+        for i , (bins , hist) in enumerate(zip(volts , spec)):
+            edges.append(self.findComptonEdges(bins , hist))
+            energy.append(bins * 0.478 / edges[i])
+
+        return(energy , spec)
+
+    def writeSpec(self , volts, spec):
+        print("writing spectrum to spec.tmp, os if something goes wrong you don't have to read through the waves again.")
+        for i , (bins , hist) in enumerate(zip(volts , spec)):
+            fname =  "spec_" + str(i)+"_.tmp"
+            np.savetxt(fname, np.c_[volts , spec] , delimiter="," , newline="\r\n" , fmt='%1.6e')
+
 
     def readFromFile(self , config ):
+        spec = []
+        volts = []
         chCount, ph, amp, tailInt, totalInt, cfd, ttt, extras, fullTime, flags, rms = getwavedata.GetWaveData(config)
-        spectrum  = np.zeros(len(self.energy))
-        for i , channel in enumerate(chCount):
-            print("looking at channel: " , i)
-            #currentInt = totalInt[i][:int(totalInt[i].size/2)]
-            hist , edges = np.histogram(ph[i][:channel] , bins=100)
-            #plt.plot(np.linspace(0,1,ph[i][:channel].size)  , ph[i][:channel])
-            plt.hist(hist , edges)
-            plt.xlabel("Pulse Height [V]")
-            plt.ylabel("Counts")
-            plt.show()
+        self.numChannels = len(chCount)
+        print("\n \n Read pulse integrated spectra from " + str(self.numChannels) + " channels. \n")
 
-        #find spectrum
-        #convert to LO
-        return(spectrum)
+        for chan , bar in zip( range(0 , len(chCount) , 2) , [1,2,3,4,5,6,7,8] ):
+             hist , bins = np.histogram(totalInt[chan+1][:chCount[chan+1]] + totalInt[chan][:chCount[chan]]  , bins=self.bins )
+             spec.append(hist)
+             volts.append(bins)
+             #plt.hist( totalInt[chan][:chCount[chan]] , bins=self.bins, label='channel: '+ str(chan) , histtype='step')
+
+        print("calibrating spectrum energy lines: " +  ' , '.join(str(e) for e in self.lines) + " MeV")
+        return(volts , spec)
+
+
+    def findComptonEdges(self , bins , hist):
+        print("Finding Compton Edges")
+        ce = [1]
+        nhist = savgol( (hist / hist.max())   , 11 , 3 )
+        diff1 = savgol(  np.diff(nhist)       , 7  , 3 )
+        diff2 = savgol(  np.diff(diff1)       , 7  , 3 )
+        #k  = np.where( diff1 == diff1.min() )
+        #k2 = np.where( diff2 == diff2.min() )
+
+        peakid  = peaks( nhist , np.arange(1,10) )
+        peakid2 = peaks( diff2 , np.arange(1,10) )
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        ax.plot( bins[1:]   , nhist            , label="spectrum"       )
+        ax.plot( bins[1:-2] , diff2            , label="2nd derivative" )
+        for pk , pk2 in zip(peakid , peakid2):
+            ax.plot( bins[pk]    , nhist[pk-1]  , '*b' , label="peak"   )
+            ax.plot( bins[pk2]   , diff2[pk2+1] , '*g' , label="inflection point"   )
+
+        plt.legend()
+
+        for line in self.lines:
+            print("Looking for " + str(line) + " MeV Compton edge.")
+
+        rs = RectangleSelector(ax, line_select_callback,
+                       drawtype='box', useblit=False, button=[1],
+                       minspanx=5, minspany=5, spancoords='pixels',
+                       interactive=True)
+
+        plt.show()
+
+        return(ce)
+
 
     def makeTestSpectrum(self , comptonErg , a , b , c):
         spectrum = np.zeros( len(self.energy) )
@@ -168,10 +341,15 @@ def visualizeConvolution(energy , experimental , simulation , broadened ):
     plt.show()
 
 if __name__ == '__main__':
-    fi  =sys.argv[1]
-    energy   = np.linspace(0 , 2 , num=1000)
-    exp = Experiment(energy)
-    wavedata = exp.readFromFile(fi)
-    #print(wavedata)
+    fi     = sys.argv[1]
+    bins   = sys.argv[2]
+    if(len(sys.argv) > 3):
+        lines = [float(x) for x in sys.argv[3:]]
+    else:
+        lines = float(input("Enter gamma line in MeV: "))
+
+    exp = Experiment(lines , bins)
+    energy , spec = exp.run(fi)
+
 
 
